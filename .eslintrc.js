140e394c0182adc3d8ff4d660211c4a23fc932e9
undefined <undefined>
2021-06-17 11:04:53 +0800

    chore($lint): add css, ts, vue code lint

---
+++ .eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: [require.resolve('@fe6/norm/src/eslint/vue3-ts')],
+};
--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
 </template>
 
 <script lang="ts" setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', 'Helvetica', 'Arial', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
