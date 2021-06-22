319e3a9fe2947bdf4a892ed48fc4ce4cfcaf0f09
iq9891 <649076408@qq.com>
2021-06-23 03:00:15 +0800

    WIP($config): add router, lint, assets, html and build configs

---
+++ src/routers/guards/index.ts
@@ -0,0 +1,25 @@
+/** @format */
+
+import { Router } from 'vue-router';
+
+// import { createProgressGuard } from './progress';
+// import { createPermissionGuard } from './permission';
+// import { createPageLoadingGuard } from './page-loading';
+// import { createTitleGuard } from './title';
+// import { createMessageGuard } from './message';
+// import { createScrollGuard } from './scroll';
+// import { createHttpGuard } from './http';
+// import { createStateGuard } from './state';
+// import { createNavGuard } from './nav';
+
+export function createGuard(router: Router) {
+  // createPageLoadingGuard(router);
+  // createNavGuard(router);
+  // createHttpGuard(router);
+  // createScrollGuard(router);
+  // createMessageGuard(router);
+  // createTitleGuard(router);
+  // createProgressGuard(router);
+  // createPermissionGuard(router);
+  // createStateGuard(router);
+}
